be81ff76318c0f04ba79625f110227616914df7e
taro-kuroda-5228 <dancing205228@gmail.com>
2022-01-10 21:11:24 +0900

    Add:sample.html

--- auto_trading/views.py
+++ auto_trading/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "auto_trading/index.html", {})
+
+
+def sample(request):
+    return render(request, "static/sample.html", {})
